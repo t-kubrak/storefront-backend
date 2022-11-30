ee6e1c0230b7293500e947ae725e134c7f773367
tkubrak <taras.kubrak@dataart.com>
2022-11-30 19:57:00 +0200

    test: order endpoints, initial

---
+++ src/handlers/tests/order_spec.ts
@@ -0,0 +1,61 @@
+import supertest from 'supertest';
+import app from '../../server';
+
+const request = supertest(app);
+
+let userId: number;
+let token: string;
+let orderId: number;
+let productId: number;
+
+describe('Test  endpoints responses', () => {
+    beforeAll(async () => {
+        const userResponse = await request.post('/users')
+            .send({first_name: 'Jack', last_name: 'Johnson', password: 'test'})
+        token = userResponse.body.token;
+        userId = userResponse.body.id;
+
+        const productResponse = await request.post('/products/')
+            .send({name: 'pencil', 'price': 50, 'category': 'office'})
+        productId = productResponse.body.id;
+    })
+
+    it('creates the order', async () => {
+        const response = await request.post(`/users/${userId}/orders`)
+            .set('Authorization', 'Bearer ' + token)
+
+        orderId = response.body.id;
+
+        expect(response.status).toBe(200);
+        expect(response.headers['content-type']).toMatch(/json/);
+        expect(Object.keys(response.body)).toContain('id');
+        expect(response.body.user_id).toEqual(userId);
+        expect(response.body.status).toEqual('current');
+    });
+
+    it('adds product to the order', async () => {
+        const response = await request.get(`/orders/${orderId}/products`)
+            .send({product_id: productId, product_quantity: 3})
+            .set('Authorization', 'Bearer ' + token)
+
+        expect(response.status).toBe(200);
+        expect(response.headers['content-type']).toMatch(/json/);
+        expect(response.body.order_id).toEqual(orderId);
+        expect(response.body.product_id).toEqual(productId);
+        expect(response.body.product_quantity).toEqual(3);
+    });
+
+    it('shows the current orders', async () => {
+        const response = await request.get(`/users/${userId}/orders`)
+            .set('Authorization', 'Bearer ' + token)
+
+        expect(response.status).toBe(200);
+        expect(response.headers['content-type']).toMatch(/json/);
+        expect(response.body.length).toBeGreaterThan(0);
+        expect(response.body[0]).toContain('id')
+        expect(response.body[0]).toContain('product_id')
+        expect(response.body[0]).toContain('product_quantity')
+        expect(response.body[0]).toContain('user_id')
+        expect(response.body[0]).toContain('status')
+    });
+});
