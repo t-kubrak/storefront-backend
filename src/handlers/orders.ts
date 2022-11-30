24014978ffa15e79a5c4776b0b0b2cebed145eef
tkubrak <taras.kubrak@dataart.com>
2022-11-30 19:55:31 +0200

    feat: add order endpoints

--- src/handlers/orders.ts
+++ src/handlers/orders.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from 'express'
 import {OrderStore} from '../models/order'
+import {verifyAuthToken} from "./users";
+// @ts-ignore
+import jwt from 'jsonwebtoken';
 
 const store = new OrderStore()
 
@@ -8,8 +11,40 @@ const current = async (req: Request, res: Response) => {
     res.json(orders)
 }
 
-const productRoutes = (app: express.Application) => {
-    app.get('/users/:id/orders', current)
+const create = async (req: Request, res: Response) => {
+    try {
+        const authorizationHeader = req.headers.authorization!
+        const token = authorizationHeader.split(' ')[1]
+        const decoded = jwt.verify(token, process.env.TOKEN_SECRET)
+        console.log(decoded)
+
+        const order = await store.create(decoded.user.id!)
+        res.json(order)
+    } catch(err) {
+        res.status(400)
+        res.json(err)
+    }
 }
 
-export default productRoutes
+const addProduct = async (req: Request, res: Response) => {
+    try {
+        const order = await store.addProduct(
+            req.body.product_quantity,
+            parseInt(req.params.id),
+            parseInt(req.body.product_id)
+        )
+
+        res.json(order)
+    } catch(err) {
+        res.status(400)
+        res.json(err)
+    }
+}
+
+const orderRoutes = (app: express.Application) => {
+    app.get('/users/:id/orders', verifyAuthToken, current)
+    app.post('/users/:id/orders', verifyAuthToken, create)
+    app.post('/orders/:id/products', verifyAuthToken, addProduct)
+}
+
+export default orderRoutes
