739b9f30d1b8943139517a9292ba605039147102
tkubrak <taras.kubrak@dataart.com>
2022-12-01 23:03:38 +0200

    fix: add missing try - catch blocks for user endpoints

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -6,13 +6,23 @@ import jwt from 'jsonwebtoken';
 const store = new UsersStore()
 
 const index = async (_req: Request, res: Response) => {
-    const users = await store.index()
-    res.json(users)
+    try {
+        const users = await store.index()
+        res.json(users)
+    } catch(err) {
+        res.status(400)
+        res.json(err)
+    }
 }
 
 const show = async (req: Request, res: Response) => {
-    const user = await store.show(parseInt(req.params.id))
-    res.json(user)
+    try {
+        const user = await store.show(parseInt(req.params.id))
+        res.json(user)
+    } catch(err) {
+        res.status(400)
+        res.json(err)
+    }
 }
 
 const create = async (req: Request, res: Response) => {
