9b886006f58d596099f8027a99394784b0bdffd7
tkubrak <taras.kubrak@dataart.com>
2022-12-01 18:09:54 +0200

    fix: check token on product create

--- src/handlers/products.ts
+++ src/handlers/products.ts
@@ -37,7 +37,7 @@ const destroy = async (req: Request, res: Response) => {
 const productRoutes = (app: express.Application) => {
     app.get('/products', index)
     app.get('/products/:id', show)
-    app.post('/products', create)
+    app.post('/products', verifyAuthToken, create)
     app.delete('/products/:id', destroy)
 }
 
